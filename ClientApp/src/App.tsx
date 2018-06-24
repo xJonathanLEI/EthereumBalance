957c1eb106ede3f4ca0065a3a1f8647e4b565743
Jonathan LEI <xJonathan@outlook.com>
2018-06-24 14:28:17 +0800

    Removed unused template pages

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 export default class App extends Component {
-  displayName = App.name
+    public displayName = App.name
 
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
-    );
-  }
+    public render() {
+        return (
+            <Route exact={true} path='/' component={Home} />
+        );
+    }
 }
